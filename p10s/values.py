54023030810a89b4aeafd5d443a137e6e97611bf
Marco Baringer <marco.baringer@deliveryhero.com>
2018-12-10 12:34:14 +0100

    cleanup dynamic values implementation

--- p10s/values.py
+++ p10s/values.py
@@ -1,11 +1,16 @@
 from pathlib import Path
 from p10s.utils import merge_dicts
 from p10s.loads import load_file
+from contextlib import contextmanager
+from copy import deepcopy
 
 
 class Values():
-    def __init__(self, values):
-        self.values = values
+    def __init__(self, values=None):
+        if values is not None:
+            self.values = values
+        else:
+            self.values = {}
 
     @classmethod
     def from_files(cls, basedir):
@@ -27,3 +32,42 @@ class Values():
             merge_dicts(values, load_file(file))
 
         return cls(values)
+
+    def copy(self):
+        return Values(values=deepcopy(self.values))
+
+    def set_value(self, key, value):
+        self.values[key] = value
+
+    def get_value(self, key, default=None):
+        return self.values.get(key, default)
+
+
+global VALUES
+VALUES = Values()
+
+
+@contextmanager
+def values(*args, **kwargs):
+    global VALUES
+    old = VALUES
+    new = old.copy()
+    for d in args:
+        for k, v in d.items():
+            new.set_value(k, v)
+    for k, v in kwargs.items():
+        new.set_value(k, v)
+    VALUES = new
+    yield
+    VALUES = old
+
+
+def value(key, default=None):
+    global VALUES
+    return VALUES.get_value(key, default)
+
+
+def set_value(key, value):
+    global VALUES
+    VALUES.set_value(key, value)
+    return value
--- tests/unit/test_values.py
+++ tests/unit/test_values.py
@@ -1,5 +1,5 @@
 import pytest
-from p10s.values import Values
+from p10s.values import Values, values, value, set_value
 
 
 def test_values_invalid_basedir(fixtures_dir):
@@ -13,3 +13,25 @@ def test_values(fixtures_dir):
                 'bottom_only': 'set at bottom',
                 'top_only': 'set at top'}
     assert expected == v.values
+
+
+def _inner():
+    return value('foo')
+
+
+def _set_foo():
+    set_value('foo', 'inner')
+
+
+def test_value_inheritence1():
+    with values(foo='top'):
+        with values(foo='bottom'):
+            assert _inner() == 'bottom'
+
+
+def test_value_inheritence2():
+    with values(foo='top'):
+        _set_foo()
+        assert _inner() == 'inner'
+        with values(foo='bottom'):
+            assert _inner() == 'bottom'
