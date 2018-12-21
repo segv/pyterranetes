330aaede414f15cdb171fa11c5ff04fa758d628c
Marco Baringer <mb@bese.it>
2018-12-21 00:19:42 +0000

    per p10s script global state

--- p10s/generator.py
+++ p10s/generator.py
@@ -2,44 +2,71 @@ import sys
 import os
 from pathlib import Path
 import runpy
+import copy
+from contextlib import contextmanager
+
 
 from p10s.base import BaseContext
 from p10s.values import values
 
 
-class Output():
-    def __init__(self, basedir):
-        self.basedir = basedir
+@contextmanager
+def _global_state(dir, extra_sys_paths):
+    here = os.getcwd()
+    sys_path = copy.copy(sys.path)
+    try:
+        os.chdir(str(dir))
+        sys.path = [str(path) for path in extra_sys_paths] + sys.path
+        yield
+    finally:
+        os.chdir(here)
+        sys.path = sys_path
+
+
+class P10SScript():
+    def __init__(self, filename):
+        self.filename = filename
+        self.base_dir = filename.parent
         self.contexts = []
+        self.pyterranetes_dir = self._find_pyterranetes_dir(filename.parent)
 
     def render(self):
-        pwd = os.getcwd()
-        os.chdir(str(self.basedir))
-        try:
-            for c in self.contexts:
+        for c in self.contexts:
+            with _global_state(dir=self.base_dir,
+                               extra_sys_paths=[self.pyterranetes_dir]):
                 c.render()
-        finally:
-            os.chdir(pwd)
+        return self
 
+    def compile(self):
+        with values({'p10s': {'file': self.filename}}):
+            with _global_state(dir=self.base_dir,
+                               extra_sys_paths=[self.pyterranetes_dir]):
+                globals = runpy.run_path(str(self.filename))
+                for value in globals.values():
+                    if isinstance(value, BaseContext):
+                        self.contexts.append(value)
+        return self
 
-class Generator():
-    def add_pyterranetes_dir(self, root):
+    def _find_pyterranetes_dir(self, root):
         here = root
         count = 0
         while True:
             maybe = here / 'pyterranetes'
             if maybe.exists() and maybe.is_dir():
-                sys.path.insert(0, "%s/" % (maybe))
+                return maybe
                 break
             if str(here) == here.root:
                 break
             here = here.parent
 
-            if count > 20:
-                raise Exception("Directroy tree too deep. aborting")
+            if count > 1000:
+                raise Exception("Directory tree too deep. aborting")
             count += 1
 
-    def p10s_files(self, root):
+
+class Generator():
+
+    def _p10s_scripts(self, root):
         if root.is_file():
             yield root
         else:
@@ -49,29 +76,8 @@ class Generator():
                     if path.suffix == '.p10s':
                         yield Path(os.path.join(dirname, file))
 
-    def compile(self, root):
-        root = Path(root).resolve()
-        if root.is_file():
-            basedir = root.parent
-        else:
-            basedir = root
-        self.add_pyterranetes_dir(basedir)
-
-        outputs = []
-        for path in self.p10s_files(root):
-            with values({'p10s': {'file': path}}):
-                output = Output(basedir=path.parent.resolve())
-                pwd = os.getcwd()
-                os.chdir(str(path.parent))
-                globals = runpy.run_path(str(path))
-                os.chdir(pwd)
-                for value in globals.values():
-                    if isinstance(value, BaseContext):
-                        output.contexts.append(value)
-                outputs.append(output)
-
-        return outputs
-
     def generate(self, root):
-        for output in self.compile(root):
-            output.render()
+        root = Path(root).resolve()
+        scripts = [P10SScript(filename=filename) for filename in self._p10s_scripts(root)]
+        for script in scripts:
+            script.compile().render()
