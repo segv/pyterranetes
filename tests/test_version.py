824451c8876300cf7dd5b9d37a890b23e3197004
Marco Baringer <marco.baringer@deliveryhero.com>
2019-01-18 01:01:34 +0100

    removed tests/unit dir, keep it simpler with all test files in ./tests/ dir

--- tests/test_version.py
+++ tests/test_version.py
@@ -4,5 +4,5 @@ import runpy
 
 
 def test_version():
-    g = runpy.run_path(str(Path(__file__).parent.parent.parent / 'p10s' / '__version__.py'))
+    g = runpy.run_path(str(Path(__file__).parent.parent / 'p10s' / '__version__.py'))
     assert __version__ == g['__version__']
