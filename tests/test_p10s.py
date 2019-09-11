c96cd96de33e6eb291d3f140dc50a7fe881a836c
Marco Baringer <mb@bese.it>
2019-09-11 15:50:53 +0200

    Removed `{tf {plan,apply},k8s}` sub commands: they create more confusion then they remove.
    The commands don't do quite enough (how do you pass `-var` or
    `-target` to terraform?), there's a lot fo dwim in then (they work
    well for exactly one use case i have, but not for the others). Given
    the cognitive overhead of knowing exactly how `p10s tf apply` works
    ust so yoiu can avoid `p10s g && terraform apply` it really doesn't
    seem worth it.

--- tests/test_p10s.py
+++ tests/test_p10s.py
@@ -19,26 +19,3 @@ def test_p10s_verbose(fixtures_dir):
                           stdout=subprocess.PIPE,
                           stderr=subprocess.STDOUT)
     assert re.match("Compiling .*?/tests/fixtures/generator_data/p10s_relative_dir/simple.p10s\n  Rendering <p10s.terraform.Context> to out/simple.tf.json in .*?/tests/fixtures/generator_data/p10s_relative_dir\n", proc.stdout.decode("utf-8"))
-
-
-def test_terraform_subcommand(fixtures_dir):
-    working_dir = fixtures_dir / 'generator_data' / 'p10s_relative_dir' / 'pwd'
-    os.chdir(str(working_dir))
-    proc = subprocess.run(["env", "PYTERRANETES_TERRAFORM=echo",
-                           "p10s", "terraform",
-                           "this", "is", "a test"],
-                          check=True,
-                          stdout=subprocess.PIPE,
-                          stderr=subprocess.STDOUT)
-    assert proc.stdout.decode("utf-8") == "this is a test\n"
-
-
-def test_tf_fail(fixtures_dir):
-    working_dir = fixtures_dir / 'generator_data' / 'p10s_relative_dir' / 'pwd'
-    os.chdir(str(working_dir))
-    proc = subprocess.run(["env", "PYTERRANETES_TERRAFORM=false",
-                           "p10s", "tf", "plan"],
-                          stdout=subprocess.PIPE,
-                          stderr=subprocess.STDOUT)
-    assert proc.returncode == 1
-    assert proc.stdout.decode("utf-8") == ""
